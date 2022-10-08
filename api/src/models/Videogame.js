45e01fb6c78f2c084aa66da1c850589b8752cf77
William Cabrera <will-f1@hotmail.com>
2022-10-08 03:57:52 -0300

    modelo videogames terminado

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,32 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
-    name: {
+    nombre: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true
     },
+    id: {
+      type: DataTypes.TEXT(5),
+      primaryKey: true
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    fecha_de_lanzamiento: {
+      type: DataTypes.DATEONLY
+    },
+    rating: {
+      type: DataTypes.FLOAT
+    },
+    plataformas: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+   
+  },
+  {
+    timestamps: false,
   });
 };
