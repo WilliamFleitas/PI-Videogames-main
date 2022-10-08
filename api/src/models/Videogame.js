74f93f683693ef8c0e0b771e95f5b190adac3093
William Cabrera <will-f1@hotmail.com>
2022-10-08 06:25:37 -0300

    comentarios agregados a videogamesmodel

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -3,6 +3,7 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
+  //define sirve para crear un modelo para la tabla
   sequelize.define('videogame', {
     nombre: {
       type: DataTypes.STRING,
