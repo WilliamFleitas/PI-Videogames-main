54e488bb06b2feeb87bb557805087d8559fad8b9
William Cabrera <will-f1@hotmail.com>
2022-10-10 02:31:21 -0300

    Genre modificado pero funcionando

--- api/src/models/Genre.js
+++ api/src/models/Genre.js
@@ -5,7 +5,7 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('genre', {
-    nombre: {
+    name: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true
