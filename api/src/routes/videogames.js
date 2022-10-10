78c092b9b3d49d78194c1b0606117268f27bc2b5
William Cabrera <will-f1@hotmail.com>
2022-10-10 02:34:28 -0300

    ruta videogames funcionando

--- api/src/routes/videogames.js
+++ api/src/routes/videogames.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const { Sequelize, Op } = require('sequelize');
 const router = express.Router();
-
-const { getGames } = require("./controllers");
+const { getGames, gameId, } = require("./controllers");
 
 router.get("/", async (req, res) => {
   try {
@@ -9,6 +9,7 @@ router.get("/", async (req, res) => {
     const response = await getGames();
 
     if (name) {
+      
       const gameFilter = response.filter((e) =>
         e.name.toLowerCase().includes(name.toLowerCase())
       );
@@ -23,6 +24,22 @@ router.get("/", async (req, res) => {
     
   } catch (error) {
     res.status(400).send(error);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const {id} = req.params;  
+    // console.log(id);
+    const response = await gameId(id);
+    const { name} = response;
+   console.log("holanda", response);
+   if(name === undefined){
+    return res.status(400).send("no se encontro el juego")
+   }
+   return res.status(200).json(response);
+  } catch (error) {
+    res.status(400).send(error)
   }
 });
 
