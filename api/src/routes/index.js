5727b53f5743ddab6854eb6ad63dce5616741d48
William Cabrera <will-f1@hotmail.com>
2022-10-10 22:56:30 -0300

    ruta genre añadida y funcionando

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const express = require("express");
 const videogamesRouter = require("./videogames");
 const platformsRouter = require("./platforms");
+const genresRouter = require("./genres");
 const {API_KEY} = process.env;
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
@@ -15,7 +16,7 @@ const router = Router();
 router.use(express.json());
 router.use("/videogames", videogamesRouter);
 router.use("/platforms", platformsRouter);
-
+router.use("/genres", genresRouter);
 
 
 
