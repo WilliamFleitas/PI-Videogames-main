cf632fd7fc206431b83991cced8805807bb803e9
William Cabrera <will-f1@hotmail.com>
2022-10-13 09:00:40 -0300

    component videogCard funcionando

--- client/src/components/VideoGameCard.jsx
+++ client/src/components/VideoGameCard.jsx
@@ -2,7 +2,20 @@ import React,{useState} from 'react';
 import {useDispatch} from 'react-redux';
 
 
-export const VideoGameCard = (props) => {
+export const VideoGameCard = ({name, image, genres, released }) => {
+
+
     const dispatch = useDispatch();
     
+
+    return (
+        <div>
+            <img src={image} alt="imagen juego" />
+            <h3>{name}</h3>
+            <h4>{genres}</h4>
+            <h5>{released}</h5>
+            <h5>hola</h5>
+            
+        </div>
+    )
 }
