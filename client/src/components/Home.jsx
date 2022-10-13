de6c839778bb04a8c976fd812ee49ca0f5615d9b
William Cabrera <will-f1@hotmail.com>
2022-10-13 20:56:56 -0300

    compo home funcionando con gamecard

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { SearchBar } from "./SearchBar";
 import { getVideogames, sortByRating } from "../redux/actions/index";
 import { VideoGameCard } from "./VideoGameCard";
+import img404 from "../imagenes/img404.jpg";
 const Home = (props) => {
   const dispatch = useDispatch();
   const allGames = useSelector((state) => state.listGames);
@@ -23,31 +24,31 @@ const Home = (props) => {
   }
 
   return (
-    <div>
-      <div>
+    <div className="homeDiv">
+      <div className="searchBarDiv">
         <SearchBar />
       </div>
-      <h1>hola: Ruta HomePage es la ruta principal</h1>
-      <button
+      <h1 className="tituloHome">hola: Ruta HomePage es la ruta principal</h1>
+      <button className="buttonGameL"
         onClick={(event) => {
           handleClick(event);
         }}
       >
         Recargar personajes
       </button>
-      <div>
-        <select>
-          <option value="asc">Ascendente</option>
-          <option value="desc">Descendente</option>
+      <div className="divFiltrados">
+        <select name="selectAscDesc" id="adf" key="adf">
+          <option key="asc" value="asc">Ascendente</option>
+          <option key="desc" value="desc">Descendente</option>
         </select>
-        <select name="ratings" onChange={(event) => handleSortByRating(event)}>
-          <option value="bestrt">Best Rating</option>
-          <option value="worstrt">Worst Rating</option>
+        <select name="selectRat" key="selectRat" onChange={(event) => handleSortByRating(event)}>
+          <option key="bestrt" value="bestrt">Best Rating</option>
+          <option key="worstrt" value="worstrt">Worst Rating</option>
         </select>
         {
          allGames && allGames.map((e) => {
           return (
-              <VideoGameCard name={e.name} image={e.image} genres={e.genres} />
+              <VideoGameCard key={e.id} id={e.id} name={e.name} platforms={e.platforms.map(e => typeof (e) === 'object' ? e.name : e ).join(', ')} released={e.released} image={e.image ? e.image: img404} genres={e.genres?.map(e => typeof (e) === 'object' ? e.name : e ).join(', ')} />
           )
            
          } )
