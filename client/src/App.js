7a6c6452f7538494abea3561771fd5d0350ee372
William Cabrera <will-f1@hotmail.com>
2022-10-13 04:39:17 -0300

    todo k

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import LandingPage from "./components/LandingPage"
+import Home from "./components/Home";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <h1>Henry Videogames</h1>
+      <Switch>
+        
+        <Route exact path="/" component={LandingPage}/>
+        <Route exact path="/home" component={Home}/>
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
