57ed08f1e606641c771fc5e51b32cc93bf447cb5
William Cabrera <will-f1@hotmail.com>
2022-10-13 04:39:29 -0300

    todo oc

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import  store  from '../src/redux/store/index.js'
+//este es mi domvirtual, aca renderizamos las cosas sin afectar al dom real
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
+   <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
