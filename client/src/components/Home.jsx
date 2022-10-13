2889ef2f1f0118b1fe79941bcb8f2f3a2cd9ba40
William Cabrera <will-f1@hotmail.com>
2022-10-13 08:59:45 -0300

    front ruta home funcionando

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,24 +1,60 @@
-import React, {useState, useEffect} from "react";
-import {useDispatch, useSelector} from 'react-redux';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { SearchBar } from "./SearchBar";
-const Homepage = (props) => {
-    
-    const dispatch = useDispatch();
-    const [gameList, setGameList] = useState("")
-    
-    
-    return (
-        <>
+import { getVideogames, sortByRating } from "../redux/actions/index";
+import { VideoGameCard } from "./VideoGameCard";
+const Home = (props) => {
+  const dispatch = useDispatch();
+  const allGames = useSelector((state) => state.listGames);
 
-            <h1>hola</h1>
-            <SearchBar/>
-            <h2>Ruta HomePage es la ruta principal</h2>
-            <div>
-                
-            </div>
-        </>
-    )
-}
+  useEffect( () => {
+   dispatch(getVideogames());
+  }, [dispatch]);
 
-export default Homepage;
+  const handleClick = (event) => {
+    event.preventDefault();
+    dispatch(getVideogames);
+  };
+
+  const handleSortByRating = (event) => {
+       event.preventDefault();
+       dispatch(sortByRating(event.target.value));
+  }
+
+  return (
+    <div>
+      <div>
+        <SearchBar />
+      </div>
+      <h1>hola: Ruta HomePage es la ruta principal</h1>
+      <button
+        onClick={(event) => {
+          handleClick(event);
+        }}
+      >
+        Recargar personajes
+      </button>
+      <div>
+        <select>
+          <option value="asc">Ascendente</option>
+          <option value="desc">Descendente</option>
+        </select>
+        <select name="ratings" onChange={(event) => handleSortByRating(event)}>
+          <option value="bestrt">Best Rating</option>
+          <option value="worstrt">Worst Rating</option>
+        </select>
+        {
+         allGames && allGames.map((e) => {
+          return (
+              <VideoGameCard name={e.name} image={e.image} genres={e.genres} />
+          )
+           
+         } )
+        }
+      </div>
+    </div>
+  );
+};
+
+export default Home;
