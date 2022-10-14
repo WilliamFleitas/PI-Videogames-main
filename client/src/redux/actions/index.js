b2b6a90bfaeab60d176feab498c2310d645d22a6
William Cabrera <will-f1@hotmail.com>
2022-10-14 05:43:13 -0300

    filtros por todos, existenteendb y api creados

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,6 +1,7 @@
 export const VIDEO_GAMES = "VIDEO_GAMES";
 export const VIDEO_GAMES_NAME = "VIDEO_GAMES_NAME";
 export const SORT_BY_RATING = "SORT_BY_RATING";
+export const FILTER_CREATED_GAME = "FILTER_CREATED_GAME";
 
 export const getVideogames = () => async (dispatch) => {
   try {
@@ -18,6 +19,7 @@ export const getVideogamesName = (name) => async (dispatch) => {
   try {
     const response = await fetch(`http://localhost:3001/videogames?name=${name}`);
     const data = await response.json();
+    
     return dispatch({ type: VIDEO_GAMES_NAME, payload: data });
   } catch (error) {
     console.log(error)
@@ -25,9 +27,17 @@ export const getVideogamesName = (name) => async (dispatch) => {
   }
 };
 
+export const filterCreatedGame = (payload) => {
+    return{
+      type: FILTER_CREATED_GAME,
+      payload,
+    }
+}
+
 export const sortByRating = (payload) => {
   return {
     type: SORT_BY_RATING,
     payload,
   };
+
 }
--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -2,10 +2,15 @@ import {
   SORT_BY_RATING,
   VIDEO_GAMES,
   VIDEO_GAMES_NAME,
+  FILTER_CREATED_GAME,
 } from "../actions/index";
 
 const initialState = {
   listGames: [],
+  videogamesF: [],
+  genres: [],
+  platforms: [],
+  videogameId: [],
 };
 
 function rootReducer(state = initialState, action) {
@@ -14,13 +19,24 @@ function rootReducer(state = initialState, action) {
       return {
         ...state,
         listGames: action.payload,
+        videogamesF: action.payload,
       };
     case VIDEO_GAMES_NAME:
       return {
         ...state,
         listGames: action.payload,
       };
-    case SORT_BY_RATING:
+    case FILTER_CREATED_GAME :
+      
+      const createdGame = action.payload === 'createdInDb' ? state.videogamesF.filter( e => e.createdInDb) : state.videogamesF.filter(e => !e.createdInDb)
+      
+      
+     return {
+          ...state,
+            listGames: action.payload === "All" ? state.videogamesF: createdGame
+     }
+      
+    case SORT_BY_RATING :
       let sortByRating =
         action.payload === "bestrt"
           ? state.listGames.sort((a, b) => a.rating - b.rating)
