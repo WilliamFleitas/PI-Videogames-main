34e28947e2ba0f6c355bd9b8084ab67549133208
William Cabrera <will-f1@hotmail.com>
2022-10-14 05:42:02 -0300

    home actualizado

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,15 +2,26 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { SearchBar } from "./SearchBar";
-import { getVideogames, sortByRating } from "../redux/actions/index";
+import { getVideogames, sortByRating, filterCreatedGame} from "../redux/actions/index";
 import { VideoGameCard } from "./VideoGameCard";
+import Paginado from "./paginado/Paginado.jsx";
 import img404 from "../imagenes/img404.jpg";
 const Home = (props) => {
   const dispatch = useDispatch();
   const allGames = useSelector((state) => state.listGames);
 
-  useEffect( () => {
-   dispatch(getVideogames());
+  const [currentPage, setCurrentPage] = useState(1);
+  const [gamesPerPage, setGamesPerPage] = useState(15);
+  const indexOfLastGame = currentPage * gamesPerPage; 
+  const indexOfFirstGame = indexOfLastGame - gamesPerPage;
+  const currentGames = allGames.slice(indexOfFirstGame, indexOfLastGame);
+
+ const paginado = (pageNumber) => {
+  setCurrentPage(pageNumber)
+ }
+
+  useEffect(() => {
+    dispatch(getVideogames());
   }, [dispatch]);
 
   const handleClick = (event) => {
@@ -19,8 +30,12 @@ const Home = (props) => {
   };
 
   const handleSortByRating = (event) => {
-       event.preventDefault();
-       dispatch(sortByRating(event.target.value));
+    event.preventDefault();
+    dispatch(sortByRating(event.target.value));
+  };
+
+  const handleFilterCreated = (event) => {
+      dispatch(filterCreatedGame(event.target.value))
   }
 
   return (
@@ -28,31 +43,64 @@ const Home = (props) => {
       <div className="searchBarDiv">
         <SearchBar />
       </div>
+
       <h1 className="tituloHome">hola: Ruta HomePage es la ruta principal</h1>
-      <button className="buttonGameL"
+
+      <button
+        className="buttonGameL"
         onClick={(event) => {
           handleClick(event);
         }}
       >
         Recargar personajes
       </button>
+
       <div className="divFiltrados">
+
         <select name="selectAscDesc" id="adf" key="adf">
-          <option key="asc" value="asc">Ascendente</option>
-          <option key="desc" value="desc">Descendente</option>
+
+          <option  value="asc">Ascendente</option>
+
+          <option  value="desc">Descendente</option>
+
         </select>
+
         <select name="selectRat" key="selectRat" onChange={(event) => handleSortByRating(event)}>
+
           <option key="bestrt" value="bestrt">Best Rating</option>
-          <option key="worstrt" value="worstrt">Worst Rating</option>
+
+          <option key="worstrt" value="worstrt"> Worst Rating</option>
+
         </select>
+        
+        <select onChange={(event) => handleFilterCreated(event)}>
+            
+            <option value="All">Todos</option>
+            <option value="createdInDb">Creados</option>
+            <option value="api">Existentes</option>
+
+        </select>
+        <Paginado gamesPerPage={gamesPerPage} allGames={allGames.length} paginado={paginado} />
+
         {
-         allGames && allGames.map((e) => {
-          return (
-              <VideoGameCard key={e.id} id={e.id} name={e.name} platforms={e.platforms.map(e => typeof (e) === 'object' ? e.name : e ).join(', ')} released={e.released} image={e.image ? e.image: img404} genres={e.genres?.map(e => typeof (e) === 'object' ? e.name : e ).join(', ')} />
-          )
-           
-         } )
-        }
+          currentGames?.map((e) => {
+            return (
+              <VideoGameCard
+                key={e.id}
+                id={e.id}
+                name={e.name}
+                platforms={e.platforms
+                  .map((e) => (typeof e === "object" ? e.name : e))
+                  .join(", ")}
+                released={e.released}
+                createdInDb={e.createdInDb}
+                image={e.image ? e.image : img404}
+                genres={e.genres
+                  ?.map((e) => (typeof e === "object" ? e.name : e))
+                  .join(", ")}
+              />
+            );
+          })}
       </div>
     </div>
   );
