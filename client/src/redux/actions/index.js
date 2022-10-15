df617765c27f99bdf1518234ed2a161a87deb196
William Cabrera <will-f1@hotmail.com>
2022-10-14 23:32:07 -0300

    action para filtrargeneros y obtener todos los juegos agregada

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -2,6 +2,9 @@ export const VIDEO_GAMES = "VIDEO_GAMES";
 export const VIDEO_GAMES_NAME = "VIDEO_GAMES_NAME";
 export const SORT_BY_RATING = "SORT_BY_RATING";
 export const FILTER_CREATED_GAME = "FILTER_CREATED_GAME";
+export const ORDER_BY_NAME = "ORDER_BY_NAME";
+export const GET_BY_GENRE = "GET_BY_GENRE";
+export const FILTER_BY_GENRES = "FILTER_BY_GENRES";
 
 export const getVideogames = () => async (dispatch) => {
   try {
@@ -27,12 +30,41 @@ export const getVideogamesName = (name) => async (dispatch) => {
   }
 };
 
+export const getByGenres = () => async  (dispatch) => {
+  try {
+    const response = await fetch(`http://localhost:3001/genres`);
+    const data = await response.json();
+  
+    return dispatch({
+      type: GET_BY_GENRE,
+      payload: data,
+    })
+  } catch (error) {
+    return error;
+  }
+
+};
+
+export const filterByGenres = (payload) => {
+  return {
+    type: FILTER_BY_GENRES,
+    payload,
+  }
+}
+
 export const filterCreatedGame = (payload) => {
     return{
       type: FILTER_CREATED_GAME,
       payload,
     }
-}
+};
+
+export const orderByName = (payload) => {
+    return {
+        type: ORDER_BY_NAME,
+        payload,
+    }
+};
 
 export const sortByRating = (payload) => {
   return {
