70f11608bf4066847147a580d04f9cc77581039a
William Cabrera <will-f1@hotmail.com>
2022-10-24 03:21:28 -0300

    cambiosdeploy

--- api/src/app.js
+++ api/src/app.js
@@ -10,7 +10,7 @@ const server = express();
 
 server.name = 'API';
 
-// server.use(cors());
+server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
