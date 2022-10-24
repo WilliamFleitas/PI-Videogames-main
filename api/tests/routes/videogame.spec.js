78b57dbe4f52152c2112fcfe80e4754b15344ace
William Cabrera <will-f1@hotmail.com>
2022-10-23 23:29:57 -0300

    test ruta videogame funcionando

--- api/tests/routes/videogame.spec.js
+++ api/tests/routes/videogame.spec.js
@@ -1,24 +1,35 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const { expect } = require('chai');
-const session = require('supertest-session');
-const app = require('../../src/app.js');
-const { Videogame, conn } = require('../../src/db.js');
+
+const session = require("supertest-session");
+const app = require("../../src/app.js");
+const { Videogame, conn } = require("../../src/db.js");
 
 const agent = session(app);
 const videogame = {
-  name: 'Super Mario Bros',
+  name: " Existen tantos juegos",
+  description: "Pero vos elegiste jugar conmigo",
+  released: "1997-06-04",
+  rating: 5,
+  platforms: ["Steam", "Pc"],
+  background_image:
+    "https://img1.ak.crunchyroll.com/i/spire2/a6e36e575f9d9d38d1cf40d6769980a31651739960_main.jpg",
+  createdInDb: true,
+  genres: ["Action", "RPG"],
 };
 
-describe('Videogame routes', () => {
-  before(() => conn.authenticate()
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  }));
-  beforeEach(() => Videogame.sync({ force: true })
-    .then(() => Videogame.create(videogame)));
-  describe('GET /videogames', () => {
-    it('should get 200', () =>
-      agent.get('/videogames').expect(200)
-    );
+describe("Videogame routes", () => {
+  before(() =>
+    conn.authenticate().catch((err) => {
+      console.error("Unable to connect to the database:", err);
+    })
+  );
+  beforeEach(() =>
+    Videogame.sync({ force: true }).then(() => Videogame.create(videogame))
+  );
+  describe("GET /videogames", () => {
+    it("should get 200 in the /videogames route", () => agent.get("/videogames").expect(200));
   });
+
+  
+
 });
