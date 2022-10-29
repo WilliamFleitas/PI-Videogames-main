78c8daf7003de514ac94cf01814a2a29bec0192b
William Fleitas <69325009+WilliamFleitas@users.noreply.github.com>
2022-10-29 07:00:18 -0300

    cambios para deploy

--- api/src/db.js
+++ api/src/db.js
@@ -6,33 +6,33 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 } = process.env;
 
-let sequelize = process.env.NODE_ENV === "production" ? new Sequelize({
-  database: DB_NAME,
-  dialect: "postgres",
-  host: DB_HOST,
-  port: 5432,
-  username: DB_USER,
-  password: DB_PASSWORD,
-  pool: {
-    max: 3,
-    min: 1,
-    idle: 10000,
-  },
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-    keepAlive: true,
-  },
-  ssl: true,
-
-}) : new Sequelize(
-     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
-      {
-      logging: false, // set to console.log to see the raw SQL queries
-      native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-      }
+let sequelize =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize({
+        database: DB_NAME,
+        dialect: "postgres",
+        host: DB_HOST,
+        port: 5432,
+        username: DB_USER,
+        password: DB_PASSWORD,
+        pool: {
+          max: 3,
+          min: 1,
+          idle: 10000,
+        },
+        dialectOptions: {
+          ssl: {
+            require: true,
+            // Ref.: https://github.com/brianc/node-postgres/issues/2009
+            rejectUnauthorized: false,
+          },
+          keepAlive: true,
+        },
+        ssl: true,
+      })
+    : new Sequelize(
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
+        { logging: false, native: false }
       );
 
 //aca nos conectamos a la base de datos videogames
@@ -69,6 +69,7 @@ const { Videogame, Genre } = sequelize.models;
 
 Videogame.belongsToMany(Genre, { through : "videogame_genre"});
 Genre.belongsToMany(Videogame, { through : "videogame_genre"});
+
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
